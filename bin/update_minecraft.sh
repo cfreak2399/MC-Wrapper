51a3c04310fea9ccce038802866f512b49cba7a4
Chris Brown <cfreak@gmail.com>
2011-06-30 00:58:13 -0500

    adding license information

--- bin/update_minecraft.sh
+++ bin/update_minecraft.sh
@@ -1,3 +1,9 @@
+# This program is free software and is provided to you with NO WARRANTY.
+# You are free to modify and distribute this program under the terms of
+# the GNU GPL v2.1. See the LICENSE file or visit 
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt for more details
+
+
 if [ $MINECRAFT_HOME != "" ]; then
 	cd $MINECRAFT_HOME
 fi
