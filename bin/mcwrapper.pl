cb02c43c046e6abb05973a20dd19a915e6fbb243
Chris Brown <cfreak@gmail.com>
2011-07-18 17:30:22 -0500

    Fixed a bug in backup

--- bin/mcwrapper.pl
+++ bin/mcwrapper.pl
@@ -7,6 +7,16 @@
 use strict;
 use IPC::Open2;
 use Proc::Daemon;
+use Getopt::Long;
+
+my $debug = '';
+my $console_mode = '';
+my $result = GetOptions( 	
+	"debug"	=>\$debug,
+	"console"	=>\$console_mode
+);
+
+$console_mode = 1 if $debug;
 
 my $java_default = `which java`;
 chomp($java_default);
@@ -27,29 +37,56 @@ my $pid_file = $ENV{MC_WRAPPER_PID} || '/var/run/mcwrapper.pid';
 my $spid_file = $ENV{MC_SERVER_PID} || '/var/run/minecraft.pid';
 my $shutdown_warning = $ENV{MINECRAFT_SHUTDOWN_WARN} || 5;
 
-print "Setting MOTD to: $MOTD\n";
+print "Setting MOTD to: $MOTD\n" if $debug;
+
 my $mc_user = $ENV{MINECRAFT_USER};
 my $mc_user_id;
 $mc_user_id = getpwnam($mc_user) if $mc_user;
 
-my %opts = ();
-$opts{work_dir} = $ENV{MINECRAFT_HOME};
-$opts{setuid} = $mc_user_id if $mc_user_id;
-my $daemon = Proc::Daemon->new(%opts);
-my $pid = $daemon->Init;
+my $pid;
+
+unless( $console_mode ) {
+	my %opts = ();
+	$opts{work_dir} = $ENV{MINECRAFT_HOME};
+	$opts{setuid} = $mc_user_id if $mc_user_id;
+	my $daemon = Proc::Daemon->new(%opts);
+	$pid = $daemon->Init;
+}
+else {
+	$pid = 0;
+}
+
+
+
+my $check_for_save = 0;
 
 unless( $pid ) {
-
+	print "HERE" if $debug;
 	$mcpid = open2( *MCOUTPUT, *MCINPUT, "$java -Xmx1024M -Xms1024M -jar $jar nogui 2>&1");
 	$| = 1;
 
-# Setup control signals for the init script
+	local $SIG{ALRM} = sub {
+		print MCINPUT "save-on\n";
+		$check_for_save = 0;
+		my $message = "WARNING: Backup failed. Minecraft didn't respond with save signal.";
+		warn $message if $debug;
+		print MCINPUT "say $message\n";
+	};
+
+	# Setup control signals for the init script
 	local $SIG{USR1} = sub {
 		print "received backup signal\n";
 		print MCINPUT "save-off\n";
 		print MCINPUT "save-all\n";
 
-		sleep 2; # wait for save to complete
+		#sleep 2; # wait for save to complete
+		$check_for_save = 1;
+		alarm 30;
+	};
+
+	my $complete_backup = sub {
+		alarm 0;
+		$check_for_save = 0;
 		my @now = (localtime)[5,4,3];
 		my $year = $now[0] + 1900;
 		my $month = $now[1] + 1;
@@ -62,6 +99,7 @@ unless( $pid ) {
 		}
 		print MCINPUT "save-on\n";
 	};
+
 	my $server_shutdown = sub {
 		if( $shutdown_warning ) {
 			print MCINPUT "say Server is shutting down in $shutdown_warning seconds.\n";
@@ -83,29 +121,37 @@ unless( $pid ) {
 
 	my $direct_next_line_to;
 	while( my $mc_said = <MCOUTPUT> ) {
-		print $mc_said;
+		print $mc_said if $debug;
+		#if( $check_for_save && $mc_said =~ /Save complete/i ) {
+		if( $check_for_save && $mc_said =~ /gobbly goop/i ) {
+			$complete_backup->();
+		}
+
 		if( $direct_next_line_to && $mc_said =~ /(Connected players:.+)$/) {
-			print "telling $direct_next_line_to the list\n";
+			print "Listing players for $direct_next_line_to\n" if $debug;
 			print MCINPUT "tell $direct_next_line_to $1\n";
 			$direct_next_line_to = '';
 		}
 
 		if( $MOTD && $mc_said =~ /\[INFO\]\s([^\[]+)\[\/[0-9\.:]+\]\slogged in/ ) {
 			my $player = $1;
+			print "giving the MOTD to $player\n" if $debug;
 			#$player =~ s/\s$//;
-			print "Detected log-in: $player\n";
 			print MCINPUT "tell $player $MOTD\n";
 		}
 
 		if( $mc_said =~ /\[INFO\]\s([^\s]+) tried command: list/ ) {
-			print "Detected list attempt\n";
 			print MCINPUT "list\n";
 			$direct_next_line_to = $1;
+			print "$direct_next_line_to issued list\n" if $debug;
 		}
 	}
 }
 else {
-	open(PIDFILE,">$pid_file") or warn "Couldn't open $pid_file: $!\n";
-	print PIDFILE $pid;
-	close(PIDFILE);
+	print "CLOSED" if $debug;
+	unless( $debug ) {
+		open(PIDFILE,">$pid_file") or warn "Couldn't open $pid_file: $!\n";
+		print PIDFILE $pid;
+		close(PIDFILE);
+	}
 }
